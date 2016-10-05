211618d378df2d784176b83b5bd01daad2ae7229
gautamc1106 <gc463@cornell.edu>
2016-10-05 19:22:06 +0000

    First Commit

--- pytree.py
+++ pytree.py
@@ -1,11 +1,33 @@
-#!/usr/bin/env python3
-import subprocess
+import os
 import sys
+import string
 
+indent = '│   '
+space = '    '
 
-# YOUR CODE GOES here
+branch = '├── '
+end_branch = '└── '
 
+file_count=0
+directory_count=0
 
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+def tree_path(path, symbol):
+    global directory_count
+    global file_count
+    all_children = sorted([c for c in os.listdir(path) if not c.startswith('.')], key=lambda s: s.strip('_').lower())
+    file_end = 0
+    children_count = 0
+    for children in all_children:
+        if(children_count < (len(all_children) - 1)):
+            print(symbol + branch + str(children))
+        else:
+            file_end = 1
+            print(symbol + end_branch+ str(children))
+        if (os.path.isdir(os.path.join(path, children))):
+            directory_count = directory_count + 1
+            if(file_end == 1):
+                tree_path(os.path.join(path, children), symbol + space)
+            else:
+                tree_path(os.path.join(path, children), symbol + indent)
+        elif(os.path.isfile(os.path.join(path, children))):
+
